677b23a930fa4dc898374017f8ea8766ef0dbc61
jerex2218 <tprivet92@gmail.com>
2025-05-17 21:29:54 +0000

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -10,30 +10,26 @@ public:
     MOCK_METHOD(void, ChangeBalance, (int diff), (override));
     MOCK_METHOD(void, Lock, (), (override));
     MOCK_METHOD(void, Unlock, (), (override));
-    MOCK_METHOD(int, id, (), (const, override)); 
+    
+    int id() const override { return id_; }
+    void setId(int id) { id_ = id; }  /
+    
+private:
+    int id_ = 0;
 };
 
 TEST(TransactionTest, MakeTransactionSuccess) {
     MockAccount from, to;
+    from.setId(1);
+    to.setId(2);
     Transaction tr;
 
-    // Устанавливаем разные ID
-    ON_CALL(from, id()).WillByDefault(testing::Return(1));
-    ON_CALL(to, id()).WillByDefault(testing::Return(2));
-
     // Ожидаемые вызовы в правильном порядке:
-    // 1. Lock обоих аккаунтов
     EXPECT_CALL(from, Lock()).Times(1);
     EXPECT_CALL(to, Lock()).Times(1);
-    
-    // 2. Кредит получателю
     EXPECT_CALL(to, ChangeBalance(300)).Times(1);
-    
-    // 3. Дебет отправителя (с комиссией)
     EXPECT_CALL(from, GetBalance()).WillOnce(testing::Return(1000));
     EXPECT_CALL(from, ChangeBalance(-301)).Times(1);
-    
-    // 4. Unlock обоих аккаунтов
     EXPECT_CALL(from, Unlock()).Times(1);
     EXPECT_CALL(to, Unlock()).Times(1);
 
