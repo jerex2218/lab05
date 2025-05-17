7a7e061bde45e61cb59f7971cfcc529a3b939b02
jerex2218 <tprivet92@gmail.com>
2025-05-17 22:54:52 +0000

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -1,39 +1,22 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include "Account.h"
-#include "Transaction.h"
-
-using namespace testing;
-
-class MockAccount : public Account {
-public:
-    MockAccount(int id, int balance) : Account(id, balance) {}
-    MOCK_METHOD(int, GetBalance, (), (const, override));
-    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
-    MOCK_METHOD(void, Lock, (), (override));
-    MOCK_METHOD(void, Unlock, (), (override));
-};
-
 TEST(TransactionTest, TransferBetweenDifferentAccounts) {
-    MockAccount from(1, 2000);
+    MockAccount from(1, 2000); // Начальный баланс достаточен для 300 + 1 (комиссия)
     MockAccount to(2, 500);
     Transaction tr;
 
-    // Устанавливаем порядок вызовов
     testing::InSequence seq;
-    
-    // 1. Блокировка аккаунтов
+
+    // Ожидаемые вызовы в правильном порядке
     EXPECT_CALL(from, Lock()).Times(1);
     EXPECT_CALL(to, Lock()).Times(1);
     
-    // 2. Кредит получателю
+    // Проверка баланса и списание с отправителя
+    EXPECT_CALL(from, GetBalance()).WillOnce(Return(2000));
+    EXPECT_CALL(from, ChangeBalance(-301)).Times(1); // 300 + 1
+    
+    // Зачисление получателю
     EXPECT_CALL(to, ChangeBalance(300)).Times(1);
     
-    // 3. Проверка баланса и дебет отправителя
-    EXPECT_CALL(from, GetBalance()).WillOnce(Return(2000));
-    EXPECT_CALL(from, ChangeBalance(-301)).Times(1); // 300 + комиссия 1
-    
-    // 4. Разблокировка
+    // Разблокировка
     EXPECT_CALL(from, Unlock()).Times(1);
     EXPECT_CALL(to, Unlock()).Times(1);
 
