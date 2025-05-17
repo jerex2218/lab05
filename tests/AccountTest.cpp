adf23cf3e2ad68c076182d7cae79e42b4a5752a4
jerex2218 <tprivet92@gmail.com>
2025-05-17 22:21:34 +0000

    Update AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -1,36 +1,15 @@
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "Account.h"
 
-class MockAccount : public Account {
-public:
-    MockAccount(int id = 0, int balance = 0) : Account(id, balance) {}
-    MOCK_METHOD(int, GetBalance, (), (const, override));
-    MOCK_METHOD(void, ChangeBalance, (int diff), (override));
-    MOCK_METHOD(void, Lock, (), (override));
-    MOCK_METHOD(void, Unlock, (), (override));
-};
-
-TEST(AccountTest, GetBalanceReturnsCorrectValue) {
+TEST(AccountTest, GetBalanceReturnsInitialValue) {
     Account acc(1, 1000);
     ASSERT_EQ(acc.GetBalance(), 1000);
 }
 
-TEST(AccountTest, ChangeBalanceThrowsWhenUnlocked) {
+TEST(AccountTest, ChangeBalanceRequiresLock) {
     Account acc(1, 1000);
     ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
-}
-
-TEST(AccountTest, ChangeBalanceWorksWhenLocked) {
-    Account acc(1, 1000);
     acc.Lock();
     ASSERT_NO_THROW(acc.ChangeBalance(100));
     ASSERT_EQ(acc.GetBalance(), 1100);
-}
-TEST(AccountTest, LockUnlockSequence) {
-    Account acc(1, 1000);
-    acc.Lock();
-    ASSERT_THROW(acc.Lock(), std::runtime_error);
-    acc.Unlock();
-    ASSERT_NO_THROW(acc.Lock());
 }
