ca4888481eceb0f47a5a8d6f118f3932c02e73fe
jerex2218 <tprivet92@gmail.com>
2025-05-17 21:45:59 +0000

    Update AccountTest.cpp

--- tests/AccountTest.cpp
+++ tests/AccountTest.cpp
@@ -4,23 +4,26 @@
 
 class MockAccount : public Account {
 public:
-    MockAccount() : Account(0, 0) {}  
+    MockAccount(int id = 0, int balance = 0) : Account(id, balance) {}
     MOCK_METHOD(int, GetBalance, (), (const, override));
     MOCK_METHOD(void, ChangeBalance, (int diff), (override));
     MOCK_METHOD(void, Lock, (), (override));
     MOCK_METHOD(void, Unlock, (), (override));
 };
 
-TEST(AccountTest, GetBalance) {
-    MockAccount acc;
-    EXPECT_CALL(acc, GetBalance()).WillOnce(testing::Return(100));
-    ASSERT_EQ(acc.GetBalance(), 100);
+TEST(AccountTest, GetBalanceReturnsCorrectValue) {
+    Account acc(1, 1000);
+    ASSERT_EQ(acc.GetBalance(), 1000);
 }
 
-TEST(AccountTest, LockUnlock) {
-    Account acc(123, 500);
+TEST(AccountTest, ChangeBalanceThrowsWhenUnlocked) {
+    Account acc(1, 1000);
+    ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
+}
+
+TEST(AccountTest, ChangeBalanceWorksWhenLocked) {
+    Account acc(1, 1000);
     acc.Lock();
     ASSERT_NO_THROW(acc.ChangeBalance(100));
-    acc.Unlock();
-    ASSERT_THROW(acc.ChangeBalance(100), std::runtime_error);
+    ASSERT_EQ(acc.GetBalance(), 1100);
 }
