8de41042821452f8b2ab0b8e9173bb665f02360a
jerex2218 <tprivet92@gmail.com>
2025-05-17 21:35:33 +0000

    Update TransactionTest.cpp

--- tests/TransactionTest.cpp
+++ tests/TransactionTest.cpp
@@ -5,30 +5,31 @@
 
 class MockAccount : public Account {
 public:
-    // Конструктор с возможностью установки ID
     MockAccount(int id = 0, int balance = 0) : Account(id, balance) {}
-    
-    // Mock-методы для виртуальных функций
     MOCK_METHOD(int, GetBalance, (), (const, override));
     MOCK_METHOD(void, ChangeBalance, (int diff), (override));
     MOCK_METHOD(void, Lock, (), (override));
     MOCK_METHOD(void, Unlock, (), (override));
-    
-    // Не mock'аем id(), используем реализацию базового класса
 };
 
 TEST(TransactionTest, MakeTransactionSuccess) {
-    // Создаем mock-аккаунты с разными ID
     MockAccount from(1, 1000);
     MockAccount to(2, 500);
     Transaction tr;
 
-    // Ожидаемые вызовы
+    // Последовательность вызовов согласно Transaction.cpp:
+    // 1. Lock обоих аккаунтов
     EXPECT_CALL(from, Lock()).Times(1);
     EXPECT_CALL(to, Lock()).Times(1);
+    
+    // 2. Credit получателю (положительное изменение)
     EXPECT_CALL(to, ChangeBalance(300)).Times(1);
+    
+    // 3. Проверка баланса и дебет отправителя
     EXPECT_CALL(from, GetBalance()).WillOnce(testing::Return(1000));
-    EXPECT_CALL(from, ChangeBalance(-301)).Times(1);
+    EXPECT_CALL(from, ChangeBalance(-301)).Times(1); // сумма + комиссия
+    
+    // 4. Unlock обоих аккаунтов
     EXPECT_CALL(from, Unlock()).Times(1);
     EXPECT_CALL(to, Unlock()).Times(1);
 
@@ -46,4 +47,13 @@ TEST(TransactionTest, SmallAmountFailure) {
     MockAccount to(2, 500);
     Transaction tr;
     EXPECT_THROW(tr.Make(from, to, 50), std::logic_error);
+}
+
+TEST(TransactionTest, InsufficientFunds) {
+    MockAccount from(1, 300);
+    MockAccount to(2, 0);
+    Transaction tr;
+
+    EXPECT_CALL(from, GetBalance()).WillOnce(testing::Return(300));
+    EXPECT_FALSE(tr.Make(from, to, 200)); // 200 + 1(fee) > 300
 }
