ae7990229877a4f1d79e368d6ee2bd493e55e317
jerex2218 <tprivet92@gmail.com>
2025-05-20 21:20:22 +0000

    Update Account.h

--- banking/Account.h
+++ banking/Account.h
@@ -5,7 +5,7 @@ class Account {
   virtual ~Account();
 
   // Virtual to test.
- int GetBalance() const { return balance_; } 
+ int GetBalance() const;
 
   // Virtual to test.
   virtual void ChangeBalance(int diff);
