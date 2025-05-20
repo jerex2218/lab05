6fe05a9d89d668ab978dddbad692c2eb876455c6
jerex2218 <tprivet92@gmail.com>
2025-05-20 20:44:32 +0000

    Update Transaction.cpp

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -50,5 +50,8 @@ bool Transaction::Debit(Account& account, int sum) {
 }
 
 void Transaction::SaveToDataBase(Account& from, Account& to, int sum) {
-   
+  std::cout << from.id() << " send to " << to.id() << " $" << sum << std::endl;
+  std::cout << "Balance " << from.id() << " is " << from.GetBalance()
+            << std::endl;
+  std::cout << "Balance " << to.id() << " is " << to.GetBalance() << std::endl;
 }
